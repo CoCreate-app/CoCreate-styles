18d2724b399d849d1fa77dbcd612926301ba4d0c
Frank Pagan <frank@cocreate.app>
2021-01-02 00:41:57 +0000

    config to publish on github packages

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   dryRun: false,
-  branches: ["master", "next"],
+  branches: ["master"],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
