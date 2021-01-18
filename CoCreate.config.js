238a6eb4e2743aae1d281e274860c93002637c41
frank pagan <frank@cocreate.app>
2021-01-18 20:54:33 +0000

    yarn docs throw error

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -1,26 +1,26 @@
-{
-    "socket": {
-        "config": {
-            "apiKey": "c2b08663-06e3-440c-ef6f-13978b42883a",
-        	"securityKey": "f26baf68-e3a9-45fc-effe-502e47116265",
-        	"organization_Id": "5de0387b12e200ea63204d6c"
+module.exports = {
+    socket: {
+        config: {
+            apiKey: "c2b08663-06e3-440c-ef6f-13978b42883a",
+        	securityKey: "f26baf68-e3a9-45fc-effe-502e47116265",
+        	organization_Id: "5de0387b12e200ea63204d6c"
         },
-        "host": "server.cocreate.app:8088"
+        host: "server.cocreate.app:8088"
     },
     
-    "sources": [
+    sources: [
         {
-            "path": "./documentation/index.html",
-            "collection": "static_html",
-            "document_id": "5f08bf3da588c11bf8ead4b3",
-            "name": "html",
+            path: "./documentation/index.html",
+            collection: "static_html",
+            document_id: "5f08bf3da588c11bf8ead4b3",
+            name: "html",
         }, 
         {
-            "collection":"website-pages",
-            "document_id":"",
-            "name":"html",
-            "domains":["ws.cocreate.app"],
-            "route":"/signup",}
-        },
+            collection:"website-pages",
+            document_id:"",
+            name:"html",
+            domains:["ws.cocreate.app"],
+            route:"/signup",
+        }
     ]
 }
